731e985cdb525cdb46d1403ec13e7a064c03f21d
new-username-now <sumaiyasiyanudeen123@gmail>
2023-11-05 22:55:33 +0530

    created a flask backend and frontend for prediction application

--- main.py
+++ main.py
@@ -2,7 +2,28 @@ import pickle
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+model = pickle.load(open('stacking_model.pkl', 'rb'))
+output = ''
 
 @app.route('/')
 def index():
-    return render_template()
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    prediction = model.predict([[request.form.get('Urine_Gravity'), 
+                                 request.form.get('Urine_pH'), 
+                                 request.form.get('Osmolality'), 
+                                 request.form.get('Conductivity'), 
+                                 request.form.get('Urea'), 
+                                 request.form.get('Calcium')]])
+    print(prediction)
+    if prediction[0] == [1]:
+        output = 'Risk of Kidney Stones'
+    if prediction == [0]:
+        output = 'No Risk of Kidney Stones'
+    return render_template('index.html', prediction_text = f'Predicted output: {output}')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
